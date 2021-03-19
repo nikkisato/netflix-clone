ba1ae63807a7698df3551825cdf3d59e7987bb6e
Nikki <nikki.satopdx@gmail.com>
2021-03-18 23:15:47 -0700

    finished front end

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
 import HomeScreen from './Component/HomeScreen/HomeScreen';
-import Nav from './Component/Nav/Nav';
-import Banner from './Component/Banner/Banner';
-import Row from './Component/Row/Row';
 
 function App() {
   return (
     <div className='app'>
-      <Nav />
       <HomeScreen />
-      <Banner />
-      {/*<Row />*/}
     </div>
   );
 }
--- src/Component/Row/Row.js
+++ src/Component/Row/Row.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Row.css';
-function Row() {
-  return <div className='row'>This is the Row</div>;
+import axios from '../../axios';
+
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovies] = useState([]);
+  const base_url = 'https://image.tmdb.org/t/p/original/';
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  return (
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='row__posters'>
+        {movies.map(
+          movie =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && ' row__posterLarge'}`}
+                key={movie.id}
+                src={`${base_url}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
