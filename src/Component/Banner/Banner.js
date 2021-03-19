381b17d1e25608efc22555eaaa6bf15972660428
Nikki <nikki.satopdx@gmail.com>
2021-03-18 17:41:09 -0700

    built the nav bar

---
+++ src/Component/Banner/Banner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Banner.css';
+function Banner() {
+  return <div className='banner'>This is the Banner</div>;
+}
+
+export default Banner;
---
+++ src/Component/Row/Row.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Row.css';
+function Row() {
+  return <div className='row'>This is the Row</div>;
+}
+
+export default Row;
